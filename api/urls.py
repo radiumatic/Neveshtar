b2a2695d5dd5a86c98d89d828ffb0e9923592939
Nima Ghasemi Por <71447191+nimagp@users.noreply.github.com>
2022-05-08 14:46:21 +0430

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('/add_like/<int:pk>/', views.add_like, name='add_like'),
-    path('/add_comment/', views.add_comment, name='add_comment'),
+    path('add_like/<int:pk>/', views.add_like, name='add_like'),
+    path('add_comment/', views.add_comment, name='add_comment'),
 
 ]
