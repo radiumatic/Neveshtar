f2af2d5d01736cc9499e68407077f0b3eba7a206
nimagp <ngsgfa2@gmail.com>
2022-07-09 15:54:44 +0430

    added docs and done some minor fixes

--- blog/static/js/post_detail.js
+++ blog/static/js/post_detail.js
@@ -126,17 +126,22 @@ ready(function() {
         comment_send.addEventListener("click", function() {
             form = document.getElementById("comment-form");
             sendForm(form);
-            //reset form inputs entered data after sending and creating a new comment div(result is not comment html)
-            
-
-
-
-
-            
-
-
-
-        }
-        );
+        });
+    }
+    const txHeight = 16;
+    const tx = document.getElementsByTagName("textarea");
+    
+    for (let i = 0; i < tx.length; i++) {
+      if (tx[i].value == '') {
+        tx[i].setAttribute("style", "height:" + txHeight + "px;overflow-y:hidden;");
+      } else {
+        tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+      }
+      tx[i].addEventListener("input", OnInput, false);
+    }
+    
+    function OnInput(e) {
+      this.style.height = "auto";
+      this.style.height = (this.scrollHeight) + "px";
     }
 });
