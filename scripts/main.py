a5de629d48a730232c92d2dff0ef28a46c9c7cab
nimagp <ngsgfa2@gmail.com>
2022-07-14 00:51:37 +0430

    fixed a bug in debug detection mechanism

--- scripts/main.py
+++ scripts/main.py
@@ -20,12 +20,14 @@ And/or are not standard to be included in used-in files
 """
 from dotenv import load_dotenv
 from os.path import exists, join
+from os import environ
 def string_to_boolean(str:str):
     return True if str.lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh'] else False
 def load_envs(parent_dir:str):
     if exists(join(parent_dir, '.env')):
+        environ["DEBUG"] = "True"
         load_dotenv()
-        Debug = True
+        
 
 
         
